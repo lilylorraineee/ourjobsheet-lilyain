ab2ddfed537d0bccc4380ad60a263a3386cbd790
lilylorraineee <lilylorraineee@gmail.com>
2026-03-31 21:05:31 +0800

    Update Lab7lily.js

--- Lab7lily.js
+++ Lab7lily.js
@@ -4,10 +4,12 @@ function updateCart(){
 
     rows.forEach(row => {
         let qty = parseFloat(row.querySelector(".qty").value) || 0;
-        let price = parseFloat(row.querySelector(".price").textContent) || 0;
+        
+        // Buang simbol $ seketika untuk buat pengiraan matematik
+        let priceText = row.querySelector(".price").textContent.replace('$', '');
+        let price = parseFloat(priceText) || 0;
 
         let amount = qty * price;
-        // Tukar unit kepada $
         row.querySelector(".amount").textContent = "$" + amount.toFixed(2);
 
         subtotal += amount;
@@ -17,16 +19,13 @@ function updateCart(){
     let shipping = 40;
     let grand = subtotal + tax + shipping;
 
-    // Paparkan hasil dalam unit $
     document.getElementById("subtotal").textContent = "$" + subtotal.toFixed(2);
     document.getElementById("tax").textContent = "$" + tax.toFixed(2);
     document.getElementById("grandtotal").textContent = "$" + grand.toFixed(2);
 }
 
-// Event listener untuk setiap kali input kuantiti berubah
 document.querySelectorAll(".qty").forEach(input => {
     input.addEventListener("input", updateCart);
 });
 
-// Jalankan fungsi pengiraan semasa halaman mula dimuatkan
 window.onload = updateCart;
